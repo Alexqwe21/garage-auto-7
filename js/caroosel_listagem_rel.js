f0e4137703cda4420629a454f1a4eab24f6fa011
ALEX SANDRO <alexsandropimenta16@gmail.com>
2026-01-25 16:04:23 -0300

    login

---
+++ js/caroosel_listagem_rel.js
@@ -0,0 +1,29 @@
+
+
+  $(document).ready(function () {
+    $('.lista_info_rel').slick({
+      slidesToShow: 4,       // Desktop
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: { slidesToShow: 3 }
+      },
+      {
+        breakpoint: 900,
+        settings: { slidesToShow: 2 }
+      },
+      {
+        breakpoint: 600,
+        settings: { slidesToShow: 1 }
+      }
+    ]
+      
+    });
+  });
+
+
+
